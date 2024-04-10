29218afc47fbdc2c8a024d661e9f7dff754d6ff4
mykomiparanum <m.miparanum@gmail.com>
2024-04-10 14:56:44 +0200

    added decreaseAmount fn

--- src/store/playerStore.ts
+++ src/store/playerStore.ts
@@ -5,6 +5,7 @@ interface PlayerState {
   bet_points: number;
   predicted_multiplier: string;
   increase_amount: (operation: "pts" | "multiplier") => void;
+  decrease_amount: (operation: "pts" | "multiplier") => void;
 }
 
 export const usePlayerStore = create<PlayerState>((set) => ({
@@ -20,6 +21,20 @@ export const usePlayerStore = create<PlayerState>((set) => ({
     if (operation === "multiplier") {
       set((state: any) => ({
         predicted_multiplier: (+state?.predicted_multiplier + 0.05).toFixed(2),
+      }));
+    }
+  },
+  decrease_amount: (operation: "pts" | "multiplier") => {
+    if (operation === "pts") {
+      set((state: any) => ({
+        bet_points:
+          state?.bet_points >= 50 ? 50 : Math.max(0, state?.bet_points - 50),
+      }));
+    }
+
+    if (operation === "multiplier") {
+      set((state: any) => ({
+        predicted_multiplier: (+state?.predicted_multiplier - 0.05).toFixed(2),
       }));
     }
   },
